fac8aabbacd229760265a5dd3a25b1bcf68238d9
elindalenback <>
2024-01-12 18:26:26 +0000

    Create initial function for action btn

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/** Connect js to the action-butten in html */
+let actionButton = document.getElementById("action-button");
+
+/** When button is clicked - function */
+actionButton.addEventListener("click", function(e) {
+    console.log("The Button has been Clicked!");
+});
