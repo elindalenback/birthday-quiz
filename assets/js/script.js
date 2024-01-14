ab5290b99544a95123dd8668a45d5d6f6c23cc63
elindalenback <>
2024-01-14 16:58:25 +0000

    Set up functions to decide correct/incorrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,6 +125,10 @@ function setNextQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerBtns.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
@@ -135,8 +139,14 @@ function resetState() {
     }
 }
 
-function selectAnswer() {
-
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
 }
 
 const questions = [
